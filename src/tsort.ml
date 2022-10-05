b2454d0a9e0b4eb904d2e5c532d09653b913ee0f
Oliver Salman <oliver@Olivers-MacBook-Pro-3.local>
2022-10-05 15:21:47 +1100

    update dfs

--- src/tsort.ml
+++ src/tsort.ml
@@ -1,42 +1,40 @@
 exception CycleFound of int list
+exception Not_found
 
-(* Finding a topological ordering of a graph using depth-first search. *)
+let graph = [(1, [2; 3]); (2, [5; 4]); (3, [6]); (4, [6]); (5, [4; 6])]
 
-(* The algorithm is based on the following idea: - We start with an empty list of nodes. -
-   We visit each node in the graph, and add it to the list of nodes if it has not been
-   visited yet. - We visit a node by visiting all its successors, and then adding the node
-   itself to the list of nodes. - If we encounter a node that has already been visited, we
-   have found a cycle in the graph. *)
+let rec is_in neighbour visited =
+  match visited with
+  | []      -> false
+  | x :: xs -> if x = neighbour then true else is_in neighbour xs
 
-let graph = [(1, [2]); (2, [3]); (3, [4; 7; 6])]
+let rec assoc x = function
+  | []          -> raise Not_found
+  | (a, b) :: l -> if compare a x = 0 then b else assoc x l
 
-(** [dfs]
+let rec fold_left f accu l =
+  match l with [] -> accu | a :: l -> fold_left f (f accu a) l
 
-    @param [graph] the graph to be traversed
-    @param [visited] the list of nodes that have already been visited
-    @param [start_node] the node to start the traversal from [explore]
-    @param [path] the topological path found so far
-    @param [visited] the list of nodes that have already been visited
-    @param [node] the node to be explored
-    @return
-      a topological ordering of the graph, or raises [CycleFound] if a cycle is found *)
+let rec map f = function
+  | []     -> []
+  | a :: l ->
+      let r = f a in
+      r :: map f l
 
 let dfs graph visited start_node =
   let rec explore path visited node =
-    if List.mem node path then raise (CycleFound path)
-    else if List.mem node visited then visited
+    if is_in node path then raise (CycleFound path)
+    else if is_in node visited then visited
     else
-      let new_path = node :: path in
-      let edges = try List.assoc node graph with Not_found -> [] in
-      let visited = List.fold_left (explore new_path) visited edges in
-      node :: visited in
+      let update_path = node :: path in
+      let update_edges = try assoc node graph with Not_found -> [] in
+      let update_visited = fold_left (explore update_path) visited update_edges in
+      node :: update_visited in
   explore [] visited start_node
 
-(** @param [graph] the graph to be traversed
-    @return a topologically sorted list *)
 let tsort graph =
-  let nodes = List.map fst graph in
-  List.fold_left (dfs graph) [] nodes
+  let nodes = map fst graph in
+  fold_left (dfs graph) [] nodes
 
 let sorted = tsort graph
 
@@ -45,4 +43,4 @@ let rec print_sorted = function
   | [x]     -> print_int x
   | x :: xs -> print_int x ; print_string " -> " ; print_sorted xs
 
-(* let _ = print_sorted sorted *)
+let () = print_sorted sorted
